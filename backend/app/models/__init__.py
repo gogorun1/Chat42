e385132075970a4cca2bacd6bbccdd7583b0354d
wenhanding <wenhanding12@gmail.com>
2026-08-08 14:28:36 +0200

    feat(f5): add notification schema

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,6 @@
+from app.models.notification import Notification, NotificationType
 from app.models.sighting import Sighting
 from app.models.user import User
 from app.models.zone import Zone
 
-__all__ = ["Sighting", "User", "Zone"]
+__all__ = ["Notification", "NotificationType", "Sighting", "User", "Zone"]
