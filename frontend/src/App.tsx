6148d375e9b78b538c9d6346b9d23a95a0b63332
shazhu <shazhu@student.42.fr>
2026-08-15 20:59:55 +0200

    add Search and Analytics tab

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,20 +10,32 @@ import { ProfilePage } from './pages/ProfilePage'
 import { SignupPage } from './pages/SignupPage'
 import { TermsOfServicePage } from './pages/TermsOfServicePage'
 import { UploadPage } from './pages/UploadPage'
+import { AnalyticsPage } from './pages/AnalyticsPage'
+import { SearchPage } from './pages/SearchPage'
+
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
+        {/* Protected routes - any authenticated user */}
         <Route element={<ProtectedRoute />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/upload" element={<UploadPage />} />
+          <Route path="/search" element={<SearchPage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/profile/:id" element={<ProfilePage />} />
           <Route element={<AdminRoute />}>
-            <Route path="/admin" element={<AdminPage />} />
-          </Route>
+             <Route path="/admin" element={<AdminPage />} />
+             </Route>
         </Route>
+
+        {/* Protected routes - admin only */}
+        <Route element={<ProtectedRoute requiredRole="admin" />}>
+          <Route path="/analytics" element={<AnalyticsPage />} />
+        </Route>
+
+        {/* Public routes */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
